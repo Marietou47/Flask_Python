b4954c8f4e9d6e4e389c3cd3903379a12278a458
Marietou47 <marietou.ba@ecoles-epsi.net>
2025-03-31 14:46:11 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ def exercice(valeur):
     etoiles = ''
     for i in range(valeur):
         etoiles += ('*' * valeur)
-        etoiles += '</b>'
+        etoiles += '</br>'
     return etoiles #comm
 
 if __name__ == "__main__":
