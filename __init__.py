978fc14c20437c4328c070ac271fa7d2a7e93415
Marietou47 <marietou.ba@ecoles-epsi.net>
2025-04-07 15:00:13 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,14 +5,12 @@ app = Flask(name)
 
 @app.route('/<path:valeurs>')
 def exercice(valeurs):
-    # Convertir les valeurs de l'URL en une liste de nombres
+
     liste_nombres = valeurs.split('/')
     liste_nombres = [int(n) for n in liste_nombres]
 
-Initialiser la variable max avec le premier nombre de la liste
     max_valeur = liste_nombres[0]
 
-Parcourir la liste et trouver la valeur maximale sans utiliser max()
     for n in liste_nombres:
         if n > max_valeur:
             max_valeur = n
