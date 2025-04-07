747a6b3a69c162b441581c204afc6c96e1d01fe9
Marietou47 <marietou.ba@ecoles-epsi.net>
2025-04-07 09:51:50 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/<int:valeur>')
 def exercice(valeur):
        pyramide = ''
-    for i in range(1, valeur + 1):
+    for i in range(1,n + 1):
            pyramide += '' * (valeur - i )
            pyramide += ''.join(str(j) for j in range(1, i + 1))
            pyramide += ''.join(str(j) for j in range (i - 1,0, -1))
