6c4979b20cbb6cee5a819dfd235da3896473c69b
Marietou47 <marietou.ba@ecoles-epsi.net>
2025-04-07 11:26:18 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,20 +1,19 @@
 from flask import Flask
 from flask import rendertemplate
 from flask import json
-
 app = Flask(name)
 
 @app.route('/<int:valeur>')
 def exercice(valeur):
     a, b = 0, 1
     sequence = [a, b]
-    for_in range(2, valeur):
-      a,b = b, a + b
-      sequence.append(b)
-      
+    for  in range(2, valeur):
+        a, b = b, a + b
+        sequence.append(b)
+
     fibonacci = '<pre>' + ', '.join(map(str, sequence)) + '</pre>'
     return fibonacci
-    
 
 if name == "main":
     app.run(debug=True)
+
