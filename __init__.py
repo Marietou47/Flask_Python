7fd5dc6a90c2d7538885aa168fb8bfdc11c37431
Marietou47 <marietou.ba@ecoles-epsi.net>
2025-04-07 11:06:59 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,25 +1,19 @@
 from flask import Flask
-from flask import render_template
-from flask import json                                                                                                                                     
-app = Flask(__name__)                                                                                                                  
+from flask import rendertemplate
+from flask import json
 
+app = Flask(name)
 
 @app.route('/<int:valeur>')
 def exercice(valeur):
-    pyramide = ''
-    for i in range(1, valeur + 1):
-        # Espaces pour centrer
-        pyramide += '&nbsp;' * (valeur - i)
-        # Chiffres qui montent
-        for j in range(1, i + 1):
-            pyramide += str(j)
-        # Chiffres qui descendent
-        for j in range(i - 1, 0, -1):
-            pyramide += str(j)
-        pyramide += '<br>'
-    return pyramide
+    a, b = 0, 1
+    sequence = [a, b]
+    for  in range(2, valeur):
+        a, b = b, a + b
+        sequence.append(b)
 
+    fibonacci = '<pre>' + ', '.join(map(str, sequence)) + '</pre>'
+    return fibonacci
 
-
-if __name__ == "__main__":
-  app.run(debug=True)
+if name == "main":
+    app.run(debug=True)
