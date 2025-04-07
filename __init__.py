e8eea327bcac1bdadc4bad8b3d017fd266849a29
Marietou47 <marietou.ba@ecoles-epsi.net>
2025-04-07 14:13:19 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,20 +1,16 @@
 from flask import Flask
 from flask import render_template
 from flask import json
-
 app = Flask(__name__)
 
-@app.route('/<int:valeur>')
-def calcul_somme(n):
-     somme = 0
-    for i in rangel(1, n + 1):
-        if i % 11 == 0:
-            continue
-         if i % 5 == 0 or i % 7 == 0:
-             somme += i
-         if somme > 5000:
-             return f"La somme a dépassé 5000.Arrêt du programme.<br>Somme finale : {somme}"
-        return f"Somme finale : {somme}"
+@app.route('/<path:valeurs>')
+def exercice(valeurs):
+    liste_nombres = valeurs.split('/')
+    liste_nombres = [int(n) for n in liste_nombres]
+    resultat = 0
+    for n in liste_nombres:
+        resultat = resultat + n
+    return str(resultat)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
