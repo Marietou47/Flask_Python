2ca3bb1ea498c0197c947de4512ee27fb1dacab4
Marietou47 <marietou.ba@ecoles-epsi.net>
2025-04-07 12:28:38 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,15 +5,16 @@ from flask import json
 app = Flask(__name__)
 
 @app.route('/<int:valeur>')
-def exercice(valeur):
-    a, b = 0, 1
-    sequence = [a, b]
-    for _ in range(2, valeur):
-        a, b = b, a + b
-        sequence.append(b)
-
-    fibonacci = '<pre>' + ', '.join(map(str, sequence)) + '</pre>'
-    return fibonacci
+def calcul_somme(n):
+     somme = 0
+    for i in rangel(1, n + 1):
+        if i % 11 == 0:
+            continue
+         if i % 5 == 0 or i % 7 == 0:
+             somme += i
+         if somme > 5000:
+             return f"La somme a dépassé 5000.Arrêt du programme.<br>Somme finale : {somme}"
+        return f"Somme finale : {somme}"
 
 if __name__ == "__main__":
     app.run(debug=True)
