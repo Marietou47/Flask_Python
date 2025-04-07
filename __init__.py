31fd070c46b0489678cc344fcca42b7e8ba23d0d
Marietou47 <marietou.ba@ecoles-epsi.net>
2025-04-07 15:17:09 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,20 +1,18 @@
 from flask import Flask
 from flask import render_template
 from flask import json
-app = Flask(name)
+
+app = Flask(__name__)
 
 @app.route('/<path:valeurs>')
 def exercice(valeurs):
-    liste_nombres = valeurs.split('/')
-    liste_nombres = [int(n) for n in liste_nombres]
+    try:
+        liste_nombres = [int(n) for n in valeurs.split('/')]
+        max_valeur = max(liste_nombres)
+        return f"Le nombre maximum est : {max_valeur}"
+    except ValueError:
+        return "Erreur : assure-toi de fournir uniquement des nombres séparés par des slashes ('/')."
 
-    max_valeur = liste_nombres[0]
-    for n in liste_nombres:
-        if n > max_valeur:
-            max_valeur = n
-
-    return str(max_valeur)
-
-if name == 'main':
+if __name__ == '__main__':
     app.run(debug=True)
-﻿
+
