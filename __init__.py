e9db2b2e69316412982d2d14dff85126ff636260
Marietou47 <marietou.ba@ecoles-epsi.net>
2025-04-07 14:56:52 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,16 +1,23 @@
 from flask import Flask
 from flask import render_template
 from flask import json
-app = Flask(__name__)
+app = Flask(name)
 
 @app.route('/<path:valeurs>')
 def exercice(valeurs):
+    # Convertir les valeurs de l'URL en une liste de nombres
     liste_nombres = valeurs.split('/')
     liste_nombres = [int(n) for n in liste_nombres]
-    resultat = 0
-    for n in liste_nombres:
-        resultat = resultat + n
-    return str(resultat)
 
-if __name__ == '__main__':
+Initialiser la variable max avec le premier nombre de la liste
+    max_valeur = liste_nombres[0]
+
+Parcourir la liste et trouver la valeur maximale sans utiliser max()
+    for n in liste_nombres:
+        if n > max_valeur:
+            max_valeur = n
+
+    return str(max_valeur)
+
+if name == 'main':
     app.run(host='0.0.0.0')
